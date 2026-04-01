364c170c0373e43fec6368b275be2cd8cfacd603
Moritz Danzer <moritz.danzer@gmail.com>
2026-04-01 17:40:56 +0200

    Add computation of correlations in time-homogeneous IDM

---
+++ R/correlation.R
@@ -0,0 +1,39 @@
+require(simIDM)
+
+# Compute correlation coefficient for PFS and OS in time-homogeneous Markovian illness-death models
+# We work on the basis of the simIDM package
+thm_correlation <- function(simIDM_thm_model) {
+  stopifnot(all(
+    class(simIDM_thm_model) ==
+      c("ExponentialTransition", "TransitionParameters")
+  ))
+  numerator <- simIDM_thm_model$hazards$h12
+  denominator_sq <- simIDM_thm_model$hazards$h01^2 +
+    2 * simIDM_thm_model$hazards$h01 * simIDM_thm_model$hazards$h02 +
+    simIDM_thm_model$hazards$h12^2
+  return(numerator / sqrt(denominator_sq))
+}
+
+### Scenario 1
+transitionTrt <- exponential_transition(h01 = 0.06, h02 = 0.3, h12 = 0.3)
+transitionPl <- exponential_transition(h01 = 0.1, h02 = 0.4, h12 = 0.3)
+thm_correlation(transitionTrt)
+thm_correlation(transitionPl)
+
+### Scenario 2
+transitionTrt <- exponential_transition(h01 = 0.3, h02 = 0.28, h12 = 0.5)
+transitionPl <- exponential_transition(h01 = 0.5, h02 = 0.3, h12 = 0.6)
+thm_correlation(transitionTrt)
+thm_correlation(transitionPl)
+
+### Scenario 3
+transitionTrt <- exponential_transition(h01 = 0.14, h02 = 0.112, h12 = 0.25)
+transitionPl <- exponential_transition(h01 = 0.18, h02 = 0.15, h12 = 0.255)
+thm_correlation(transitionTrt)
+thm_correlation(transitionPl)
+
+### Scenario 4
+transitionTrt <- exponential_transition(h01 = 0.18, h02 = 0.06, h12 = 0.17)
+transitionPl <- exponential_transition(h01 = 0.23, h02 = 0.07, h12 = 0.19)
+thm_correlation(transitionTrt)
+thm_correlation(transitionPl)
